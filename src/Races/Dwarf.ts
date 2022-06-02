2544051a46d3f9343feb3f86ab8884753f9f36cb
Carlos Barros <carlos_dbs@hotmail.com>
2022-06-02 17:28:42 -0300

    feat:implements instance count

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -2,6 +2,7 @@ import Race from './Race';
 
 class Dwarf extends Race {
   private _maxLifePoints:number;
+  private static _dwarfInstace = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
@@ -10,6 +11,11 @@ class Dwarf extends Race {
 
   get maxLifePoints(): number {
     return this._maxLifePoints;
+  }
+
+  static createdRacesInstances() {
+    this._dwarfInstace += 1;
+    return this._dwarfInstace;
   }
 }
 
