9a9d4361712eccff2ca1acbb274be65f7820dbb0
Carlos Barros <carlos_dbs@hotmail.com>
2022-06-02 21:18:57 -0300

    feat:creates Nectromancer Archetype

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,23 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+class Necromancer extends Archetype {
+  private static _instance = 0;
+  private _energyType: EnergyType;  
+  
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._instance += 1;
+    return this._instance;
+  }
+}
+
+export default Necromancer;
--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,7 +1,9 @@
 import Archetype from './Archetype';
 import Mage from './Mage';
+import Necromancer from './Necromancer';
 
 export default Archetype;
 export {
   Mage,
+  Necromancer,
 };
