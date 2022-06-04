421df10cbeeaa4cfc2ce8104c4a210605e86145b
Carlos Barros <carlos_dbs@hotmail.com>
2022-06-04 13:40:26 -0300

    feat:creates Monster

---
+++ src/Monster.ts
@@ -0,0 +1,38 @@
+import SimpleFighter from './Fighter/SimpleFighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this.lifePoints;
+    
+    if (damage > 0) this._lifePoints -= damage;
+    
+    let lifePoint = this.lifePoints;
+
+    if (lifePoint <= 0) this._lifePoints = -1;
+    
+    lifePoint = this.lifePoints;
+    return lifePoint;
+  }
+}
+
+export default Monster;
